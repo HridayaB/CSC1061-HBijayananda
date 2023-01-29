8556e5e84f99c062d475666751e5e7818586112b
Hridaya Bijayananda:= <hrbcrrvp@gmail.com>
2023-01-28 19:30:13 -0700

    Fan assignment done

--- FanClass/src/TestFan.java
+++ FanClass/src/TestFan.java
@@ -11,12 +11,12 @@ public class TestFan
 		System.out.println ( "Default Fan: " );
 		System.out.println ( defaultFan.toString ( ) );
 		
-		Fan fan1 = new Fan ( fanSpeed.FAST, true,10.0, "yellow" );
+		Fan fan1 = new Fan ( fanSpeed.FAST, true, 10.0, "yellow" );
 		System.out.println ( "Fan object 1: " );
 		System.out.println ( fan1.toString ( ) );
 		
-		Fan fan2 = new Fan ( fanSpeed.MEDIUM, false,5.0, "blue" );
-		System.out.println ( "Fan object 2: " );
+		Fan fan2 = new Fan ( fanSpeed.MEDIUM, false, 5.0, "blue" );
+		System.out.println ( "Fan object 2: " ); 
 		System.out.println ( fan2.toString ( ) );
 		
 	} // end of main
